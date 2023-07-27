a82c38fc626f47470e3821f1d55302ef1dc1da82
sohyuno <osh_1004@naver.com>
2023-07-27 22:12:24 +0900

    main1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const side = ()=>{
+let onSide = document.querySelector('.on-side');
+let main = document.querySelector('main');
+
+
+onSide.classList.toggle('on');
+main.classList.toggle('mainon');
+
+
+}
